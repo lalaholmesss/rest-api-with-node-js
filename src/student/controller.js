c277def1a44cb7754389916908159e5bb5ea6457
lalaholmesss <lalaholmesart00@gmail.com>
2025-04-25 23:59:11 +0400

    Add delete and put methods

--- src/student/controller.js
+++ src/student/controller.js
@@ -2,21 +2,24 @@ const { response } = require('express');
 const pool = require('../../db.js');
 const queries = require('./queries.js');
 
+// Get all students 
 const getStudents = (req, res) => {
     pool.query(queries.getStudents, (error, results) => {
         if(error) throw error;
         res.status(200).json(results.rows);
-    })
+    });
 };
 
+// Get specific student data by typing their id in the url
 const getStudentById = (req, res) => {
     const id = parseInt(req.params.id);
     pool.query(queries.getStudentById, [id], (error, results) => {
         if(error) throw error;
         res.status(200).json(results.rows);
-    })
+    });
 };
 
+// Add new student data to the database
 const addStudent = (req, res) => {
     const { name, email, age, dob } = req.body;
 
@@ -30,7 +33,41 @@ const addStudent = (req, res) => {
             if(error) throw error;
             res.status(201).send("Student created successfully."); // status 200 - success for reading, updtading, deleting;
                                                                    // status 201 - success for creating data
-        })
+        });
+    });
+};
+
+// Delete specific student data by typing their id in the url
+const deleteStudentById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        if(!results.rows.length) {
+            res.send("Student doesn't exist in the database.");
+        }
+
+        pool.query(queries.deleteStudentById, [id], (error, results) => {
+            if(error) throw error;
+            res.status(200).send("Student removed successfully.");
+        });
+    });
+};
+
+// Update specific student data(name) by typing their id in the url
+const updateStudent = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name } = req.body;
+
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        if(!results.rows.length) {
+            res.send("Student doesn't exist in the database.");
+        }
+
+        pool.query(queries.updateStudent, [name, id], (error, results) => {
+            if(error) throw error;
+            res.status(200).send("Student successfully updated."); // why 200 and not 201?
+        });
+       
     });
 };
 
@@ -38,4 +75,6 @@ module.exports = {
     getStudents, 
     getStudentById,
     addStudent,
+    deleteStudentById,
+    updateStudent,
 };
--- src/student/routes.js
+++ src/student/routes.js
@@ -6,7 +6,7 @@ const controller = require('./controller');
 router.get("/", controller.getStudents);
 router.post("/", controller.addStudent);
 router.get("/:id", controller.getStudentById);
-
-
+router.delete("/:id", controller.deleteStudentById);
+router.put("/:id", controller.updateStudent);
 
 module.exports = router;
